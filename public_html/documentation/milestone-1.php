67562361421b8bfee0a24a3f01565f788f103a5b
amy skidmore <askidmore1@cnm.edu>
2017-04-26 22:40:24 -0600

    Completed project for milestone-one, and apparently i'm old, it says my html is dep. althoug, it still works, so i'm leaving it. Ask about it.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,23 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title><bold>Amy's Portfolio</bold></title>
+	</head>
+	<body>
 
-Purpose: This project will be a professional portfolio concentrating on web development in order to gain business.
+<strong><center>Milestone One</center></strong>
 
-Audience: This site will be designed and directed toward professionals such as; web developers, clients, startup founders, hiring managers, recruiters, business owners, and peers.
+<br><strong>Purpose:</strong> This project will be a professional portfolio concentrating on web development in order to gain business.</br>
 
-Goal: The primary goal for this site is to garner name recognition, direct business, provide employment opportunities, and allow for connection/collaboration inside the IT community. As a secondary goal I would like this site to become a forum for displaying my work and increasing my social media presence.
+		<br> <strong>Audience:</strong> This site will be designed and directed toward professionals such as; web developers, clients, startup founders, hiring managers, recruiters, business owners, and peers.
 
-Persona:
+		<strong>Goal:</strong> The primary goal for this site is to garner name recognition, direct business, provide employment opportunities, and allow for connection/collaboration inside the IT community. As a secondary goal I would like this site to become a forum for displaying my work and increasing my social media presence.
 
-Name: Eric Sanders
-Age:32
+	<br><strong>Persona:</strong>
 
-Profession: Hiring manager for a small business specializing in Contracting and design. The business has had a serious increase in sales and is now looking to extend their marketing strategy into an online forum. He is looking to hire a full time web develop that is comfortable with producing work encompassing the front and backend.
+		<br><strong>Name:</strong> Eric Sanders
+		<br><strong>Age:</strong>32
 
-Technology: Mr. Sanders is a dedicated Apple user who has a MacBook Pro, an Ipad Pro, and an Iphone 7 that he uses both professionally and personally.
+		<br><strong>Profession:</strong> Hiring manager for a small business specializing in Contracting and design. The business has had a serious increase in sales and is now looking to extend their marketing strategy into an online forum. He is looking to hire a full time web develop that is comfortable with producing work encompassing the front and backend.
 
-Attitudes and Behaviors: Mr. Sanders has very little time to select candidates and as he is comfortable with techonlogy would prefer resumes and portfolios that are submitted online. He spends only a few minutes verifying a candidates potential for filling a position and only a little more to check out a candidates portfolio site.
+		<br><strong>Technology:</strong> Mr. Sanders is a dedicated Apple user who has a MacBook Pro, an Ipad Pro, and an Iphone 7 that he uses both professionally and personally.
 
-Frustrations and Needs: Mr. Sanders is the only hiring manager for his company and as they are experiencing an exponential growth he is overworked and frustrated with trying to fill so many positions in a short span of time. He needs a candidate comfortable with working alone, accepting an entry level salary, and who is willing to maintain his/her work for an extended length of time.
+		<br><strong>Attitudes and Behaviors:</strong> Mr. Sanders has very little time to select candidates and as he is comfortable with techonlogy would prefer resumes and portfolios that are submitted online. He spends only a few minutes verifying a candidates potential for filling a position and only a little more to check out a candidates portfolio site.
 
-Goal: Mr. Sanders goal is to select the correct person to fill the job very quickly.
+		<br><strong>Frustrations and Needs:</strong> Mr. Sanders is the only hiring manager for his company and as they are experiencing an exponential growth he is overworked and frustrated with trying to fill so many positions in a short span of time. He needs a candidate comfortable with working alone, accepting an entry level salary, and who is willing to maintain his/her work for an extended length of time.
 
-Use Case: It is 8:05 a.m., and Mr. Sanders is in his office using his MacBook Pro to check portfolios against the web developer job vacancy. As he has received my application for employment, and has placed me on a short list for possible personal, he is currently searching my portfolio site. Mr. Sanders has several other positions to fill this morning and as a result will not even consider a confusing or hard to access portfolio, immediately, dropping that candidate. He will be looking for the relevant information and qualifications to be easy to access and clearly displayed.
+		<br><strong>Goal:</strong> Mr. Sanders goal is to select the correct person to fill the job very quickly.
+
+		<br><strong>Use Case:</strong> It is 8:05 a.m., and Mr. Sanders is in his office using his MacBook Pro to check portfolios against the web developer job vacancy. As he has received my application for employment, and has placed me on a short list for possible personal, he is currently searching my portfolio site. Mr. Sanders has several other positions to fill this morning and as a result will not even consider a confusing or hard to access portfolio, immediately, dropping that candidate. He will be looking for the relevant information and qualifications to be easy to access and clearly displayed.
+	</body>
+</html>
