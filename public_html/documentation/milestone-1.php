fdc0b117bce8846db4ccd4439ee5b2789291948c
amy skidmore <askidmore1@cnm.edu>
2017-04-26 19:19:36 -0600

    test

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,29 @@
+Purpose, Audience, Goal
+This is a brief, high-level overview of what you intend to create. Your Personal Website Project should be relevant to your web development education, business venture, personal growth, professional field, or an area of personal interest. Have an idea to create something different? Cool! Please discuss it with your instructors.
+
+In your milestone-1.php file, using valid HTML markup, compose no more than one brief statement for each of the following:
+
+Purpose: What would you like your PWP to help you accomplish? This is a high-level goal akin to a Mission Statement.
+Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of? Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?
+Goal: This is should be a quantifiable goal if possible. For example, a goal for your PWP could be:
+To acquire full or part-time employment or an internship.
+To increase sales and/or conversion rates for an existing business.
+To attract freelance clients.
+To serve as a gallery to showcase and promote your work.
+To increase the reach of a new or established social media presence (as measured via analytics tools).
+Hint: Use the W3C Markup Validator Service to check your HTML for errors: http://validator.w3.org/
+
+Persona
+In your milestone-1.php file, define one persona that represents the primary target audience for your Personal Website. More information on how to create a persona for your project here: http://www.webcredible.com/blog-reports/web-usability/personas.shtml
+
+Your persona should feature the following at minimum:
+
+Name
+A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site.
+Goal(s) regarding the use of your site.
+Additional information regarding your persona may also include:
+
+Frustrations your persona may be facing.
+Additional information your persona may need in order to achieve their goal.
+Use Case
+A use case should be based on your persona’s motivations and goals. Your use case should detail the time, location, technology used, and the purpose or goal of your Persona's use of your site.
