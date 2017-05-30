926a1abab08ebac366882a9f1f386eb6b4e9cbea
amy skidmore <askidmore1@cnm.edu>
2017-05-30 15:43:38 -0600

    @rlewis37 - directory and head tag cleanup

--- public_html/index.php
+++ public_html/index.php
@@ -1,106 +1,167 @@
 <!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
+<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="utf-8"/>
 		<!--Tells the browser that the page is written in UTF-8 unicode. This is for internationalization.-->
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 		<!--This tag is for Internet Explorer, and it allows us to specify what version of IE to render page in. "IE=edge" tells IE to display our content in the highest mode available, which avoids "IE Compatibility Mode" bugs.-->
 
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<!--This is the responsive meta tag. This is used to scale and size our content relative to the "viewport" - which is essentially is the visible portion of screen itself. This tag also sets the inital zoom scale to 1:1. This <meta> tag is required when creating a responsive web site.-->
+
 		<meta name="description" content="PWP">
 		<meta name="author" content="Amy Skidmore">
-		<title>Amy Skidmore</title>
-		<!-- Bootstrap Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-		<!--This loads our Bootstrap CSS file directly from maxcdn.-->
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
-		<!--This tag is optional. It loads the default Bootstrap theme directly from maxcdn.-->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme compiled/min/css-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+				crossorigin="anonymous"/>
+
 		<!-- Font Awesome -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<!-- My Custom CSS -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+				rel="stylesheet"/>
+
+		<!--custom css-->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
+
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<!--This is an HTML Conditional Comment. These are conditional statements that are read and executed only by Internet Explorer versions 5 - 9, and are now officially deprecated. We use this block of code to serve up a couple fo specific JavaScript files just for users of older versions of IE. Here, we will load html5shiv.js and respond.js in IE 8 and below. Respond.js enables CSS media query support for IE 6-8. HTML5 shiv enables styling of HTML5 elements in IE8 and below. (IE8 and below does not allow unknown elements to be styled without JavaScript.)-->
+		<!--This is an HTML Conditional Comment. These are conditional statements that are read and executed only by
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins, This loads the jQuery JavaScript library from Google's CDN. jQuery required for many of Bootstrap's built-in functionality.) -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+		<script src="js/jquery.easing.min.js" type="text/javascript"></script>
 
+		<!-- jQuery Form, Validate, Additional Methods -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"
+				  type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"
+				  type="text/javascript"></script>
 
-		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included, This is Bootstrap's JavaScript file, which is required if you want to feature many of Bootstrap's built-in components such as modal windows, dropdown menus, and transitions. This file, which is being directly loaded from maxcdn, includes all the Bootstrap components. -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-		<link href="css/scrolling-nav.css" rel="stylesheet">
-		<script src="js/jquery.easing.min.js"></script>
-		<script src="js/scrolling-nav.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
+		<!-- Custom JAvascript -->
+		<script src="js/scrolling-nav.js" type="text/javascript"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<title>Amy Skidmore</title>
 
 	</head>
-	<body>
-		<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top"
-				<header>
-					<!--navbartop-->
-					<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-						<div class="container">
-							<div class="navbar-header page-scroll">
-								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-								<a class="navbar-brand page-scroll" href="#page-top">
-									<div class="name-button">Amy<span> Skidmore's</span>
-										<div class="name-button">Web Design & Development</div>
-									</div>
-								</a>
-							</div>
-							<!-- links, forms, and content for nav bar -->
-							<div class="collapse navbar-collapse navbar-ex1-collapse">
-								<ul class="nav navbar-nav navbar-right">
-									<li class="hidden active">
-									</li>
-									<li>
-										<a class="page-scroll" href="#page-top"</a>
-									</li>
-									<li>
-										<a class="page-scroll" href="#about">About</a>
-									</li>
-									<li>
-										<a class="page-scroll" href="#portfolio">Portfolio</a>
-									</li>
-									<li>
-										<a class="page-scroll" href="#contact">Contact</a>
-									</li>
-								</ul>
-							</div>
-							<!--Collapse the navbar-->
-						</div>
-					</nav>
-				</header>
-		<main>
 
-		</main>
-<!--Intro Image-->
-		<section>
-			<section id="intro" class="intro-section"
-					<div class="container">
-						<div class="row">
-							<div class="col-lg-12"></div>
-							<img class="my images" src="image/intro.jpg"/>
-						</div>
+	<header>
+		<nav>
+			<div class="navbar navbar-default navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header page-scroll">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="#page-top" class="navbar-brand page-scroll">
+							<div class="name"><span>Amy Skidmore</span>
+							</div>
+						</a>
 					</div>
-	</section>
+				</div>
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav navbar-right">
 
-							</div>
-						</div>
-					</nav>
-				</header>
+						<li>
+							<a class="page-scroll" href="#about">About</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#Portfolio">Portfolio</a>
+						</li>
+							<li>
+							<a class="page-scroll" href="contact">Contact</a>
+						</li>
+					</ul>
+				</div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container -->
+		</nav>
+	</header>
+	<body>
+		<!--Bootstrap-Jumbotron-->
+		<div class="jumbotron extra-jumbotron">
+			<Hr>Amy's<hr><bold>Web Development & Design</bold></hr></p>
+		</div>
+			<div class="container">
+		</div>
+
+			<!--about me -->
+
+		<section>
+			<id="about" class="about-section"></id>
+				<div class="row text-center">
+					<div class="col-lg-3 col-xs-6">
+					<H2>About</H2>
+					<p>sldkfjs
+					skdjflsld
+					laskdfjls</p>
+					<a href="#about"/a>
+				</div>
+			<div class="col-lg-3 col-xs-6">
+				<a href="#profile">
+				<img class="img-round img-responsive" src="image/profile.jpg" alt="map displaying data"></a>
+			</div>
+
+
+				</div>
+		</section>
+
+
+		<!-- Portfolio section -->
+		<section id="portfolio" class="portfolio-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<h1 class="section-heading">PORTFOLIO</h1>
+					</div>
+				</div>
+				<p>
+					blah
+				</p>
+			</div>
+
+		</section>
+
+		<!--Contact-->
+		<section id="contact" class="contact-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12" text-center></div>
+					<h1 class="section-title">Contact</h1>
+					<h3 class="text-muted">Feel Free to Contact Me For Further Information</h3>
+					</span>
+				</div>
+			</div>
+		</section>
+
+
 	</body>
+
 </html>
